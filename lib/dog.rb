448eec9f9acfadb620c0708da8396b8c84319b09
shiruiuiui <shirui.us@gmail.com>
2018-07-12 21:03:52 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,10 @@
 # dog.rb
+class Dog
+  def initialize(name)
+  @name=name
+  end
+end
+fido=Dog.new("Fido")
+snoopy=Dog.new("Snoopy")
+lassie=Dog.new("Lassie")
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,8 @@
 # person.rb
+class Person
+  def initialize(name)
+      @name=name
+  end
+end
+adele_goldberg=Person.new("Adele Goldberg")
+alan_kay=Person.new("Alan Kay")
